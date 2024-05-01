ff64150bbbee285aeae8e9cb916ccb9a9fde2963
kishmwangi <kishmwangi31@gmail.com>
2024-05-01 12:54:07 +0300

    Flatiron Stock Exchange

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function PortfolioContainer({ stocks, onRemoveStock }) {
+  const stockList = stocks.map((stock) => (
+    <Stock key={stock.id} stock={stock} onStockClick={onRemoveStock} />
+  ));
+
   return (
     <div>
-      <h2>Stocks</h2>
-      {/* render stock list here*/}
+      <h2>My Portfolio</h2>
+      {stockList}
     </div>
   );
 }
 
-export default StockContainer;
+export default PortfolioContainer;
